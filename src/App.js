13827c9494e0ea39673a84c0c4c39f20f122a97b
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-05-20 17:50:43 +0300

    Added form and todoList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-      <h1>Hello react</h1>
+      <header>
+        <h1>Oguzhan's Todo List {inputText} </h1>
+      </header>
+      <Form onSetInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
