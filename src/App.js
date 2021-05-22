cc815e5aa2cc53ddff82fd54f15764733fa72c0f
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-05-22 20:14:24 +0300

    Added localStorage

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
   const [status, setStatus] = useState("all");
   const [filterTodos, setFilterTodos] = useState([]);
   useEffect(() => {
+    getLocalTodos();
+  }, []);
+  useEffect(() => {
+    saveLocalStorage();
     filterHandler();
   }, [todos, status]);
   const filterHandler = () => {
@@ -24,6 +28,19 @@ function App() {
         break;
     }
   };
+
+  const saveLocalStorage = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todoLocal);
+    }
+  };
+
   return (
     <div className="App">
       <header>
